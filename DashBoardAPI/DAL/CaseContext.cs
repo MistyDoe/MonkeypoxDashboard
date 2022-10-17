c996e90ee0b010f07590bb1768b8824cd64414ae
Migle Urbonaite <mig.urbonaite@gmail.com>
2022-10-17 14:45:08 +0200

    Added data models and sqlite configurations

---
+++ DashBoardAPI/DAL/CaseContext.cs
@@ -0,0 +1,19 @@
+﻿using DashBoardAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DashBoardAPI.DAL
+{
+    public class CaseContext : DbContext
+    {
+        public DbSet<Case> Cases { get; set; }
+
+        protected override void OnConfiguring (
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(
+                "Data Source=cases.db");
+        }
+    }
+}
+
---
+++ DashBoardAPI/Models/Case.cs
@@ -0,0 +1,10 @@
+﻿namespace DashBoardAPI.Models
+{
+    public class Case
+    {
+        public int Id { get; set; }
+        public DateTime DateRep { get; set; }
+        public string? CountryExp { get; set; }
+        public int ConfCases { get; set; }
+    }
+}
